bb82ac792732f127deef6451360829eeddb01134
steelvelveteen <joeyvico@gmail.com>
2020-04-25 12:24:11 +0930

    Added a listener to listen for screen size changes and apply styles in real time

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ function init(){
     console.log('DOM has been loaded');
     let query = window.matchMedia('(max-width: 599px)');
 
-    if (query.matches) {
-        console.log('Media matches: max-width: 599px');
-        document.querySelector('h1').style.color = "red";
-    } else {
-        console.log('Nope');
-        document.querySelector('h1').style.color = "black";
-    }
+    // if (query.matches) {
+    //     console.log('Media matches: max-width: 599px');
+    //     document.querySelector('h1').style.color = "red";
+    // } else {
+    //     console.log('Nope');
+    //     document.querySelector('h1').style.color = "black";
+    // }
+
+    // This works!!
+    query.addEventListener('change', (q) => {
+        if (q.matches) {
+            document.querySelector('h1').style.color = "red";
+            console.log('Media matches: max-width: 599px');
+        } else {
+            document.querySelector('h1').style.color = "black";
+            console.log('Nope');
+        }
+    });
 }
