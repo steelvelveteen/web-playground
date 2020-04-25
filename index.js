f2253f50976b588911861a21f732faf253728b39
steelvelveteen <joeyvico@gmail.com>
2020-04-25 11:34:01 +0930

    Played with media queries from javascript

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+    console.log('DOM has been loaded');
+    let query = window.matchMedia('(max-width: 599px)');
+
+    if (query.matches) {
+        console.log('Media matches: max-width: 599px');
+        document.querySelector('h1').style.color = "red";
+    } else {
+        console.log('Nope');
+        document.querySelector('h1').style.color = "black";
+    }
+}
