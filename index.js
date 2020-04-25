b6cc1721133ced43abb58e643afe849576aa94f1
steelvelveteen <joeyvico@gmail.com>
2020-04-25 17:02:38 +0930

    Very basic polymorphism

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    makeSound() {
+        console.log('Generic animal sound');
+    }
+}
+
+class Lion extends Animal {
+    constructor(name, kingdom) {
+        super(name);
+        this.kingdom = kingdom;
+    }
+
+    makeSound() {
+        console.log("Roooaaargghg");
+    }
+}
+
+const genericAnimal = new Animal("Generica animal");
+genericAnimal.makeSound();
+const savanahLion = new Lion("Mufassa");
+savanahLion.makeSound();
