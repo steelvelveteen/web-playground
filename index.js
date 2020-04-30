ef318cec483049d79f519d56442c0ce690642368
steelvelveteen <joeyvico@gmail.com>
2020-04-30 18:02:49 +0930

    Little example with aysnc and await

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log("Hello World");
+// Follows the example on branch callbacks-vs-promises
+// loginUser(email, password) => getUserVideos(email)
+
+loginUser = (email, password) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Loggin in ...');
+            if (password === 68989) {
+                console.log('Successfullly logged in with user email: ' + email);
+                resolve({ useremail: email });
+            } else {
+                reject(new Error('User email not found'));
+            }
+        }, 2000);
+    });
+};
+
+getUserVideos = (email) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Getting videos....from useremail: ' + email);
+            if (email === 'joeyvico@gmail.com') {
+                resolve(['Interstellar', 'Blade Runenr 2049','Minority Report'])
+            } else {
+                reject(new Error('Wrong email or email not provided'));
+            }
+        }, 2250);
+    });
+};
+
+// loginUser('joeyvico@gmail.com', 68989)
+// .then(user => getUserVideos(user.useremail))
+// .then(videos => console.log(videos))
+// .catch(error => console.log('Exception handled correctly: ' + error));
+
+
+//Async and await
+ displayUser = async() => {
+    const loggedInUser = await loginUser('joeyvico@gmail.com', 68989);
+    const videos = await getUserVideos(loggedInUser.useremail);
+    console.log(videos);
+}
+
+displayUser();
