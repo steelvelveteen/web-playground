76dde83f0d169746930e33b1d954771f3a7c04c7
steelvelveteen <joeyvico@gmail.com>
2020-05-03 08:57:45 +0930

    Basic destructuring

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
+const personOne = {
+    name: 'Joey',
+    age: 45,
+    address: {
+        city: 'Adelaide',
+        state: 'SA'
+    }
+}
+
+const personTwo = {
+    name: "Sonoya Mizuno",
+    age: 34,
+    address: {
+        city: 'Tokyo',
+        state: 'Japan'
+    }
+}
+const first = personTwo.name.split(' ')[0];
+const last = personTwo.name.split(' ')[1];
+console.log(first);
+console.log(last);
+
+const { name: fullName, address: {state: country} } = personTwo;
+console.log(fullName);
+console.log(country);
