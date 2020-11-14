bda9fc5c2a49f751273e08bcc005e8608038d3d2
steelvelveteen <joeyvico@gmail.com>
2020-11-14 17:57:55 +1030

    Examples of data-attributes usage

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Hello Webpack Playground');
+var article = document.querySelector('#electric-cars');
 
-});
+console.log(article.dataset.columns);
+console.log(article.dataset.indexNumber);
+console.log(article.dataset.parent);
+article.dataset.columns = 15;
+console.log(article.dataset.columns);
 
