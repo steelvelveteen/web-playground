879c86a4ca81b9e25eb26c2bb606698bcfab796f
steelvelveteen <joeyvico@gmail.com>
2020-11-21 08:46:54 +1030

    Arrays part two

--- index.js
+++ index.js
@@ -1,5 +1,103 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Hello Webpack Playground');
+const newReleases = [{
+        id: 70111470,
+        title: "Die Hard",
+        boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: 4.0,
+        bookmark: [],
+    },
+    {
+        id: 654356453,
+        title: "Bad Boys",
+        boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: 5.0,
+        bookmark: [{
+            id: 432534,
+            time: 65876586
+        }],
+    },
+    {
+        id: 65432445,
+        title: "The Chamber",
+        boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: 4.0,
+        bookmark: [],
+    },
+    {
+        id: 675465,
+        title: "Fracture",
+        boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: 5.0,
+        bookmark: [{
+            id: 432534,
+            time: 65876586
+        }],
+    },
+];
 
-});
+// Use filter and map to select id of all videos with
+// a rating of 5.0
+const highRatedVideosId = newReleases
+    .filter((video) => video.rating === 5.0)
+    .map((vid) => ({
+        id: vid.id
+    }));
+console.log(highRatedVideosId);
 
+/// ****************************** ///
+var movieLists = [{
+        name: "New Releases",
+        videos: [{
+                id: 70111470,
+                title: "Die Hard",
+                boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+                uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+                rating: 4.0,
+                bookmark: [],
+            },
+            {
+                id: 654356453,
+                title: "Bad Boys",
+                boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+                uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+                rating: 5.0,
+                bookmark: [{
+                    id: 432534,
+                    time: 65876586
+                }],
+            },
+        ],
+    },
+    {
+        name: "Dramas",
+        videos: [{
+                id: 65432445,
+                title: "The Chamber",
+                boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+                uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+                rating: 4.0,
+                bookmark: [],
+            },
+            {
+                id: 675465,
+                title: "Fracture",
+                boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+                uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+                rating: 5.0,
+                bookmark: [{
+                    id: 432534,
+                    time: 65876586
+                }],
+            },
+        ],
+    },
+];
+// Flatten the movieLists array into an array of video ids
+const flattenedListOfVideosIds = movieLists.forEach((movieList) =>
+    movieList.videos.map((video) => video.id)
+);
+console.log("Flattened List of video ids");
+console.log(flattenedListOfVideosIds);
