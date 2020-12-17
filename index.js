f48f40b1e568f6805f4263e4787d504abf47adec
steelvelveteen <joeyvico@gmail.com>
2020-12-17 21:03:08 +1030

    Completed everything. Was a great learning experience still at my age. Next stop: Netflix clone

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Hello Webpack Playground');
 });
+/* .container .main-nav .main-menu */
+const menuBtn = document.querySelector('.menu-btn');
+const mainMenu = document.querySelector('.main-menu');
 
+menuBtn.addEventListener('click', (e) => {
+    mainMenu.classList.toggle('show');
+});
+
+mainMenu.addEventListener('click', () => {
+    mainMenu.classList.toggle('show');
+});
